f056c165157b378b0d406ceb44643f7aa83fde3a
esmameral <esma.meral@gmail.com>
2022-10-21 10:03:08 +0300

    method examples

--- java.examples/src/week4/examples/BankAccount.java
+++ java.examples/src/week4/examples/BankAccount.java
@@ -1,29 +1,60 @@
 package week4.examples;
 
 public class BankAccount {
-    private String owner;
-    private int accountNumber=123;
-    private double balance=100;
-    
-    public String getOwner() {
-        return owner;
-    }
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-    public double getBalance() {
-        return balance;
-    }
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-    
-    
+
+	private String owner;
+	private int accountNumber = 123;
+	private double balance = 100;
+
+	public double computeInterestAmount(double rate, int duration) {
+
+		// double interestAmount = getBalance()*duration*rate/1200;
+		// double interestAmount = this.balance*duration*rate/1200;
+		double interestAmount = balance * duration * rate / 1200;
+		return interestAmount;
+
+	}
+
+	public double computeInterestAmountFor(double amount, double rate, int duration) {
+		double interestAmount = amount * duration * rate / 1200;
+		return interestAmount;
+
+	}
+
+	public double formatAmount(double amount) {
+		return (int) (amount * 1000) / 1000.0;
+
+//    	(int)(729166.666666666)=>729166
+//    			729166/1000.0 = 729.166
+//    	
+//    	729.1666666666666
+//    	
+//    	729.166
+
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
 }
--- java.examples/src/week4/examples/TestBankAccount.java
+++ java.examples/src/week4/examples/TestBankAccount.java
@@ -5,11 +5,21 @@ public class TestBankAccount {
     public static void main(String[] args) {
         BankAccount myAccount = new BankAccount();
         myAccount.setOwner("Kerem Demircan");
-        myAccount.setBalance(1000);
+        myAccount.setBalance(4509);
 
+        double interest=myAccount.computeInterestAmount(12.5, 7);
+        
         System.out.println(myAccount.getOwner());;
         System.out.println(myAccount.getBalance());
         System.out.println(myAccount.getAccountNumber());
+        System.out.println("Interest Amount(Original):"+interest);
+        System.out.println("Interest Amount:"+myAccount.formatAmount(interest));
+        
+        //Find interest amount for 10000
+        double interestNew=myAccount.computeInterestAmountFor(10000.0,12.5, 7);
+        System.out.println("WHEN Amount:"+10000+" Rate:"+12.5+" Duration:"+7);
+        System.out.println("INTEREST:"+interestNew);
+        
 
     }
 
