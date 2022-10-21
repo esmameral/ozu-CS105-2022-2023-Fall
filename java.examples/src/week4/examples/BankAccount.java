191ac6b1c6cd901219df6489f13e49e5149dc785
esmameral <esma.meral@gmail.com>
2022-10-21 10:34:46 +0300

    method examples

--- java.examples/src/week4/examples/BankAccount.java
+++ java.examples/src/week4/examples/BankAccount.java
@@ -4,33 +4,26 @@ public class BankAccount {
 
 	private String owner;
 	private int accountNumber = 123;
-	private double balance = 100;
+	private double balance;// = 100; 
 
 	public double computeInterestAmount(double rate, int duration) {
-
 		// double interestAmount = getBalance()*duration*rate/1200;
 		// double interestAmount = this.balance*duration*rate/1200;
 		double interestAmount = balance * duration * rate / 1200;
 		return interestAmount;
-
 	}
 
 	public double computeInterestAmountFor(double amount, double rate, int duration) {
 		double interestAmount = amount * duration * rate / 1200;
 		return interestAmount;
-
 	}
 
 	public double formatAmount(double amount) {
 		return (int) (amount * 1000) / 1000.0;
-
 //    	(int)(729166.666666666)=>729166
 //    			729166/1000.0 = 729.166
-//    	
 //    	729.1666666666666
-//    	
 //    	729.166
-
 	}
 
 	public String getOwner() {
@@ -53,8 +46,26 @@ public class BankAccount {
 		return balance;
 	}
 
-	public void setBalance(double balance) {
+	private void setBalance(double balance) {
 		this.balance = balance;
+	}
+
+	public void withdraw(double amount) {
+		if(amount<=balance) {
+			balance=balance-amount;
+		}else {
+			System.out.println("Insufficient balance !!!");
+		}
+		
+		//Others ways:
+		//setBalance(balance-amount);
+		//setBalance(getBalance()-amount);
+		
+	}
+
+	public void deposit(double amount) {
+		balance=balance+amount;
+		
 	}
 
 }
--- java.examples/src/week4/examples/TestBankAccount.java
+++ java.examples/src/week4/examples/TestBankAccount.java
@@ -3,15 +3,29 @@ package week4.examples;
 public class TestBankAccount {
 
     public static void main(String[] args) {
+        System.out.println("------------- Creating Bank Account instance -------------");
+
         BankAccount myAccount = new BankAccount();
         myAccount.setOwner("Kerem Demircan");
-        myAccount.setBalance(4509);
-
+        //myAccount.setBalance(4509); //setBalance is private
+        System.out.println("Owner:"+myAccount.getOwner());;
+        System.out.println("Balance:"+myAccount.getBalance());
+        System.out.println("Account Number:"+myAccount.getAccountNumber());
+        System.out.println();
+        System.out.println("------------- Deposit/Withdraw Examples -------------");
+        
+        myAccount.deposit(900);
+        System.out.println("After deposit:"+myAccount.getBalance());
+        myAccount.withdraw(100);
+        System.out.println("After withdraw:"+myAccount.getBalance());
+        
+        myAccount.withdraw(10000);
+        System.out.println("After second withdraw:"+myAccount.getBalance());
+        System.out.println();
+        System.out.println("------------- Interest amount examples -------------");
         double interest=myAccount.computeInterestAmount(12.5, 7);
         
-        System.out.println(myAccount.getOwner());;
-        System.out.println(myAccount.getBalance());
-        System.out.println(myAccount.getAccountNumber());
+        
         System.out.println("Interest Amount(Original):"+interest);
         System.out.println("Interest Amount:"+myAccount.formatAmount(interest));
         
@@ -20,7 +34,10 @@ public class TestBankAccount {
         System.out.println("WHEN Amount:"+10000+" Rate:"+12.5+" Duration:"+7);
         System.out.println("INTEREST:"+interestNew);
         
-
+        myAccount.withdraw(100.0);
+        myAccount.deposit(300.0);
+        
+        //check balance value     
     }
 
 }
