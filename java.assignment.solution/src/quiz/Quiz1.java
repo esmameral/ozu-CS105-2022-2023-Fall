348717560d6892cfa7eabf1826552facd0e4b6c6
esmameral <esma.meral@gmail.com>
2022-10-26 01:05:43 +0300

    Quiz1 solution is added

---
+++ java.assignment.solution/src/quiz/Quiz1.java
@@ -0,0 +1,25 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer value:");
+		int N=input.nextInt();
+		boolean number13Found=false;
+		for (int i = 1; i <=N; i++) {
+			int newNumber=10+(int)(Math.random()*10);
+			System.out.println("next number:"+newNumber);
+			if(newNumber==13)
+				number13Found=true;
+		}
+		if(number13Found)
+			System.out.println("We saw at least one 13!");
+		else
+			System.out.println("No 13 was seen");
+
+	}
+
+}
