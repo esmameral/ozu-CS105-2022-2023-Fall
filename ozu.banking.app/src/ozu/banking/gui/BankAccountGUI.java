88d69d87c36b659d404cca3cbb014ddfa3133180
esmameral <esma.meral@gmail.com>
2023-01-04 11:23:37 +0300

    Jtable added

--- ozu.banking.app/src/ozu/banking/gui/BankAccountGUI.java
+++ ozu.banking.app/src/ozu/banking/gui/BankAccountGUI.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -25,12 +26,11 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 	private JLabel title;
 	private JLabel lblName;
 	private JTextField txtName;
-	
+
 	private JLabel lblBalance;
 	private JTextField txtBalance;
 	private JLabel lblTrxAmount;
 	private JTextField txtTrxAmount;
-	
 
 	private JButton btnCreate;
 	private JButton btnPost;
@@ -39,9 +39,10 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 	private JLabel lblResponse;
 	private JComboBox<String> comboTrxType;
 	private JLabel lblTrxInfo;
-	
+
 	private BankAccount account;
-	private JTable table;
+	private JTable jTable;
+	private JScrollPane scrollPane;
 
 	public BankAccountGUI() {
 		setTitle("Account Form");
@@ -95,16 +96,15 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 		btnClear.setLocation(290, 200);
 		btnClear.addActionListener(this);
 		c.add(btnClear);
-		
-		comboTrxType=new JComboBox<String>();
+
+		comboTrxType = new JComboBox<String>();
 		comboTrxType.addItem("Deposit");
 		comboTrxType.addItem("Withdraw");
 		comboTrxType.setLocation(270, 262);
 		comboTrxType.setFont(new Font("Arial", Font.PLAIN, 15));
 		comboTrxType.setSize(100, 20);
 		c.add(comboTrxType);
-		
-		
+
 		lblTrxAmount = new JLabel("Transaction Amount");
 		lblTrxAmount.setFont(new Font("Arial", Font.PLAIN, 20));
 		lblTrxAmount.setSize(250, 20);
@@ -116,7 +116,7 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 		txtTrxAmount.setSize(150, 20);
 		txtTrxAmount.setLocation(110, 262);
 		c.add(txtTrxAmount);
-		
+
 		btnPost = new JButton("Post");
 		btnPost.setFont(new Font("Arial", Font.PLAIN, 15));
 		btnPost.setSize(100, 20);
@@ -137,15 +137,27 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 		lblResponse.setSize(500, 25);
 		lblResponse.setLocation(100, 500);
 		c.add(lblResponse);
-		
+
 		lblTrxInfo = new JLabel("Transaction Information");
 		lblTrxInfo.setBounds(100, 308, 184, 30);
 		getContentPane().add(lblTrxInfo);
+
+		String[][] data = { { "100", "Deposit", "21.12.2022" }, { "250", "Withdraw", "03.01.2023" } };
+
+		// Column Names
+		String[] columnNames = { "Amount", "Type", "Date" };
+
+		// Initializing the JTable
+		jTable = new JTable(data, columnNames);
 		
-		table = new JTable();
-		table.setBounds(30, 40, 200, 300);
+		scrollPane = new JScrollPane(jTable);
+		scrollPane.setBounds(100, 340, 200, 300);
 		
-		getContentPane().add(table);
+		scrollPane.setSize(337, 100);
+		scrollPane.setLocation(100, 375);
+
+		getContentPane().add(scrollPane);
+
 		setVisible(true);
 	}
 
@@ -153,44 +165,42 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		lblResponse.setText("");
 		if (e.getSource() == btnCreate) {
-			account=new BankAccount(txtName.getText());
+			account = new BankAccount(txtName.getText());
 			try {
 				account.increaseBalance(Double.parseDouble(txtBalance.getText()));
 				System.out.println(account);
 				txtLog.setText(account.toString());
 				lblResponse.setText("Account created successfully..");
-			}catch(Exception exception) {
+			} catch (Exception exception) {
 				lblResponse.setText("Please enter a valid double value!!");
 			}
-	
-		}
-		else if (e.getSource() == btnPost) {
+
+		} else if (e.getSource() == btnPost) {
 			Transaction trx;
 			try {
-				if(comboTrxType.getSelectedItem().equals("Withdraw")){
-					trx=new WithdrawTransaction(Double.parseDouble(txtTrxAmount.getText()));
-					
-				}else {
-					trx=new DepositTransaction(Double.parseDouble(txtTrxAmount.getText()));
+				if (comboTrxType.getSelectedItem().equals("Withdraw")) {
+					trx = new WithdrawTransaction(Double.parseDouble(txtTrxAmount.getText()));
+
+				} else {
+					trx = new DepositTransaction(Double.parseDouble(txtTrxAmount.getText()));
 
 				}
 				lblTrxInfo.setText(trx.toString());
 				account.post(trx);
-				txtLog.setText(txtLog.getText()+"\n"+account.toString());
-				txtLog.setText(txtLog.getText()+"\n"+trx.toString());
+				txtLog.setText(txtLog.getText() + "\n" + account.toString());
+				txtLog.setText(txtLog.getText() + "\n" + trx.toString());
 				txtTrxAmount.setText("");
 				comboTrxType.setSelectedItem("Deposit");
-				
+
 			} catch (Exception e1) {
 				lblResponse.setText(e1.getMessage());
-			} 
-			
-		}
-		else if (e.getSource() == btnClear) {
+			}
+
+		} else if (e.getSource() == btnClear) {
 			txtName.setText("");
 			txtBalance.setText("");
 			lblResponse.setText("");
-			//txtLog.setText("");
+			// txtLog.setText("");
 
 		}
 
@@ -200,5 +210,3 @@ public class BankAccountGUI extends JFrame implements ActionListener {
 		new BankAccountGUI();
 	}
 }
-
-
